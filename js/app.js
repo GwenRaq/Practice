28924849649e7b68e7cd776aea872f114af16f48
Corneliousm808 <corneliousmooring@gmail.com>
2017-03-31 09:22:03 -1000

    pau

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-if(document.getElementById('button').clicked == true)
-{
+//if(document.getElementById('button').clicked == true)
+//{
+//}
+
+function alerts(){
    alert("Hi");
 }
-
 //html is <input id="button" type="submit" name="button" value="enter"/>
